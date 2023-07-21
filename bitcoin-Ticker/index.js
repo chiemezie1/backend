0ca9ad92e8e2f1dad8ffa11da3cb8ef72af6555d
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-21 13:56:47 +0100

    index.js

---
+++ bitcoin-Ticker/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
