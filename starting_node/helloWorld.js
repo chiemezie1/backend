516e6b6392eda3381471f0c947c56d4d8262b53e
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-23 20:33:16 +0100

    update

--- starting_node/helloWorld.js
+++ starting_node/helloWorld.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req,res){
- res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello world!');
+ res.writeHead(200, { 'Content-Type': ' text/html' });
+ res.end('<p>Hello world!</p>');
 }).listen(3002);
 console.log('Server started on localhost:3002; press Ctrl-C to terminate....');
