af87b895bb60642f56771153ae03043336ae0a0a
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-28 13:43:43 +0100

    mongoose

--- simple_site/app.js
+++ simple_site/app.js
@@ -2,6 +2,7 @@
  const express = require('express');
  const bodyParser = require('body-parser');
  const ejs = require('ejs');
+ const mongoose = require('mongoose');
 
  const app = express();
 
@@ -9,3 +10,21 @@
  app.use(express.static('public'));
  app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+
+ app.get('/', (req, res) => {
+   res.render('home');
+ });
+
+ app.get(`/register`, (req, res) => {
+   res.render('register');
+ });
+
+ app.get('/login', (req, res) => {
+   res.render('login');
+ });
+
+ app.listen(3000, () => {
+   console.log('Server started on port 3000');
+ });
